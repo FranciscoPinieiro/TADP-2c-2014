3adc75df8ed1fd89a3a3eec322c9e5b63cffe806
mazzeog <mazzeo.gn@gmail.com>
2014-09-09 19:55:27 -0300

    Punto 1. Faltaria bindear los metodos del prototipo con los del objeto cuando se hace objeto.set_prototype(prototipo)

--- Test/proto_traits_spec.rb
+++ Test/proto_traits_spec.rb
@@ -2,6 +2,95 @@ require 'rspec'
 require_relative '../src/domain/proto_trait_tipos'
 describe 'Prototype' do
 
+
+it 'Asigno propiedad a guerrero' do
+
+  guerrero = PrototypedObject.new
+  guerrero.set_property( :energia, 100)
+  expect(guerrero.energia).to eq(100)
+end
+
+it 'Cuando un guerrero ataca a otro' do
+
+  guerrero = PrototypedObject.new
+  guerrero.set_property( :energia, 100)
+  guerrero.set_property(:potencial_defensivo, 10)
+  guerrero.set_property(:potencial_ofensivo, 30)
+
+  guerrero.set_method(:atacar_a,
+                      lambda {
+                          |otro_guerrero|
+                        if(otro_guerrero.potencial_defensivo < self.potencial_ofensivo)
+                          otro_guerrero.recibe_danio(self.potencial_ofensivo - otro_guerrero.potencial_defensivo)
+                        end
+                      });
+
+  guerrero.set_method(:recibe_danio, lambda { | ataque| self.energia -= ataque})
+  otro_guerrero = guerrero.clone #clone es un metodo que ya viene definido en Ruby
+  guerrero.atacar_a otro_guerrero
+  expect(otro_guerrero.energia).to eq(80)
+
+end
+
+
+
+=begin
+it 'Cuando seteo un prototipo se obtienen todos sus metodos' do
+
+  guerrero = PrototypedObject.new
+  guerrero.set_property( :energia, 100)
+  guerrero.set_property(:potencial_defensivo, 10)
+  guerrero.set_property(:potencial_ofensivo, 30)
+
+  otro_guerrero = PrototypedObject.new
+  otro_guerrero.set_property( :energia, 100)
+  otro_guerrero.set_property(:potencial_defensivo, 10)
+  otro_guerrero.set_property(:potencial_ofensivo, 30)
+
+  otro_guerrero.set_method(:atacar_a,
+                             proc {
+                                 |otro_guerrero|
+                               if(otro_guerrero.potencial_defensivo < self.potencial_ofensivo)
+                                 otro_guerrero.recibe_danio(self.potencial_ofensivo - otro_guerrero.potencial_defensivo)
+                               end
+                             });
+  otro_guerrero.set_method(:recibe_danio, lambda { | ataque| self.energia -= ataque})
+
+  guerrero.set_prototype(otro_guerrero)
+  expect(otro_guerrero.methods(false)).to eq(guerrero.methods(false))
+
+end
+
+it 'guerrero ataca a su prototipo' do
+
+  guerrero = PrototypedObject.new
+  guerrero.set_property( :energia, 100)
+  guerrero.set_property(:potencial_defensivo, 10)
+  guerrero.set_property(:potencial_ofensivo, 30)
+
+  otro_guerrero = PrototypedObject.new
+  otro_guerrero.set_property( :energia, 100)
+  otro_guerrero.set_property(:potencial_defensivo, 10)
+  otro_guerrero.set_property(:potencial_ofensivo, 30)
+
+  otro_guerrero.set_method(:atacar_a,
+                           proc {
+                               |otro_guerrero|
+                             if(otro_guerrero.potencial_defensivo < self.potencial_ofensivo)
+                               otro_guerrero.recibe_danio(self.potencial_ofensivo - otro_guerrero.potencial_defensivo)
+                             end
+                           });
+  otro_guerrero.set_method(:recibe_danio, lambda { | ataque| self.energia -= ataque})
+
+  guerrero.set_prototype(otro_guerrero)
+  guerrero.atacar_a(otro_guerrero)
+  expect(otro_guerrero.energia).to eq(80)
+end
+=end
+
+
+
+=begin
   it 'asigno un metodo a un guerrero' do
     guerrero = PrototypedObject.new
     guerrero.set_method(:atacar_a,
@@ -31,7 +120,6 @@ describe 'Prototype' do
     })
     un_guerrero = Guerrero.new(100, 30, 10)
     expect(un_guerrero.energia).to eq(100)
-
   end
 
 
@@ -51,5 +139,7 @@ describe 'Prototype' do
     guerrero.atacar_a otro_guerrero
     expect(otro_guerrero.energia).to eq(80)
   end
+=end
+
 
 end
--- src/domain/proto_trait_tipos.rb
+++ src/domain/proto_trait_tipos.rb
@@ -1,18 +1,31 @@
-
-
 class PrototypedObject
-  include PrototypeConstructor
+  attr_accessor :interested
+
+  def interested
+    @interested = @interested || []
+    @interested
+  end
+
  def set_method (a_method, a_block )
    self.singleton_class.send(:define_method,
                               a_method,a_block)
+   self.interested.each{|a_interested| a_interested.set_method( a_method, a_block) }
 
  end
   def set_property (a_attr, a_value)
-      self.instance_variable_set(a_attr, a_value)
+      self.instance_variable_set("@#{a_attr}", a_value)
+      self.set_method( a_attr, lambda { self.instance_variable_get("@#{a_attr}") } )
+      self.set_method( "#{a_attr}=", lambda { |something| self.instance_variable_set("@#{a_attr}",
+                                                          something)})
   end
 
-  def set_prototype (an_object)
+  def set_prototype (a_prototype)
+    a_prototype.set_interested(self)
+  end
 
+  def set_interested( a_interested)
+    @interested << a_interested
+    # aca habria que bindear los metodos del prototipo con los del objeto
   end
 end
 
